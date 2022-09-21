f6470608dc5198cbb8731d1e2e7be5fb19634027
Melissa <watergirl_r@yahoo.com>
2022-09-21 15:35:29 -0600

    added authorization to header and exposeHeaders = authorization

--- p2-scape-goats/src/main/java/com/revature/tarotcards/p2scapegoats/controllers/melissa/CategoryController.java
+++ p2-scape-goats/src/main/java/com/revature/tarotcards/p2scapegoats/controllers/melissa/CategoryController.java
@@ -1,10 +1,15 @@
 package com.revature.tarotcards.p2scapegoats.controllers.melissa;
 
+import com.revature.tarotcards.p2scapegoats.dtos.john.request.JohnNewCategoryRequest;
 import com.revature.tarotcards.p2scapegoats.dtos.request.melissa.NewCategoryRequest;
 import com.revature.tarotcards.p2scapegoats.dtos.request.melissa.NewRoleRequest;
+import com.revature.tarotcards.p2scapegoats.dtos.request.melissa.NewUserRequest;
 import com.revature.tarotcards.p2scapegoats.models.melissa.Categories;
 import com.revature.tarotcards.p2scapegoats.models.melissa.Roles;
 import com.revature.tarotcards.p2scapegoats.services.melissa.CategoryService;
+import com.revature.tarotcards.p2scapegoats.utils.melissa.custom_exceptions.InvalidRequestException;
+import com.revature.tarotcards.p2scapegoats.utils.melissa.custom_exceptions.ResourceConflictException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +25,9 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @CrossOrigin
+    //@ExceptionHandler(value = {ResourceConflictException.class, InvalidRequestException.class})
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/addcategory", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody String addcategory(@RequestBody NewCategoryRequest request){
 
@@ -29,5 +37,16 @@ public class CategoryController {
     @GetMapping(value = "/getall", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<Categories> getAll(){
         return categoryService.getAll();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public @ResponseBody ResourceConflictException handleResourceConflictException(ResourceConflictException e){
+        return e;
+    }
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody InvalidRequestException handleInvalidRequestException(InvalidRequestException e){
+        return e;
     }
 }
