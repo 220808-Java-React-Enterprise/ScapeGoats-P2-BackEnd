dda24608cea049455bc6d66723dd424dd859c8b5
Melissa <watergirl_r@yahoo.com>
2022-09-22 17:05:04 -0600

    corrected getAllReadingsById

--- p2-scape-goats/src/main/java/com/revature/tarotcards/p2scapegoats/repositories/john/JohnReadingRepository.java
+++ p2-scape-goats/src/main/java/com/revature/tarotcards/p2scapegoats/repositories/john/JohnReadingRepository.java
@@ -9,12 +9,12 @@ import java.util.List;
 
 public interface JohnReadingRepository extends CrudRepository<Readings, String> {
 
-    @Query(value = "SELECT * FROM readings WHERE id = ?1", nativeQuery = true)
-    List<Readings> findReadingById(String id);
+    @Query(value = "SELECT * FROM readings WHERE user_id = ?1", nativeQuery = true)
+    List<Readings> findReadingById(String userid);
 
 
     @Query(value = "DELETE FROM readings WHERE id = ?1", nativeQuery = true)
-    void deleteReadingById(String id);
+    void deleteReadingById(String userid);
 
 //FKrwj5v69sohlqxpioou1871h4v
 }
