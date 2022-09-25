bc6f9e4bb6a3f3d2552ab744df3cf3422abc79a3
Melissa <watergirl_r@yahoo.com>
2022-09-24 21:30:13 -0600

    added UserServiceTest

--- p2-scape-goats/src/test/java/com/revature/tarotcards/p2scapegoats/services/melissa/UserServiceTest.java
+++ p2-scape-goats/src/test/java/com/revature/tarotcards/p2scapegoats/services/melissa/UserServiceTest.java
@@ -6,13 +6,16 @@ import com.revature.tarotcards.p2scapegoats.repositories.melissa.RoleRepository;
 import com.revature.tarotcards.p2scapegoats.repositories.melissa.UserRepository;
 import com.revature.tarotcards.p2scapegoats.utils.melissa.custom_exceptions.InvalidRequestException;
 import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import static org.mockito.Mockito.*;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,20 +26,35 @@ class UserServiceTest {
 
     UserService testService;
 
-      //creating a mock class of UserService
+    //creating a mock class of UserService
     private UserRepository userRepositoryMock;
 
     private RoleRepository roleRepositoryMock;
     private Users mockUser;
     private NewUserRequest mockRequest;
 
-    @Before //run before each test
+    @BeforeEach //run before each test
     public void setUp() {
         userRepositoryMock = mock(UserRepository.class);
         roleRepositoryMock = mock(RoleRepository.class);
         testService = new UserService(userRepositoryMock, roleRepositoryMock);
         mockUser = mock(Users.class);
         mockRequest = mock(NewUserRequest.class);
+        when(mockRequest.getUsername()).thenReturn("username");
+        when(mockRequest.getPassword()).thenReturn("Passw0rd$");
+        when(mockRequest.getFirstname()).thenReturn("firstname");
+        when(mockRequest.getLastname()).thenReturn("lastname");
+        when(mockRequest.getEmail()).thenReturn("email@test.com");
+        when(mockRequest.getRole_id()).thenReturn("456");
+
+    }
+    @AfterEach
+    public void setdown(){
+        userRepositoryMock = null;
+        roleRepositoryMock = null;
+        testService = null;
+        mockUser = null;
+        mockRequest = null;
     }
 
     // @Test tells the JUnit that the public void method in which it is used can run as a test case
@@ -50,31 +68,69 @@ class UserServiceTest {
 
     @Test
     public void test_getAll_fail(){
-        when(userRepositoryMock.findAll()).thenReturn(Arrays.asList(mockUser));
+        when(userRepositoryMock.findAll()).thenReturn(new ArrayList<>());
         InvalidRequestException thrown = Assertions.assertThrows(InvalidRequestException.class, () -> {
             testService.getAll();
         });
         verify(userRepositoryMock, times(1)).findAll();
-        Assertions.assertEquals("no users found", thrown.getMessage());
+        Assertions.assertEquals("No users found", thrown.getMessage());
     }
 
     @Test
-    void addUser() {
+    void test_addUser_succeed() {
+        when(mockRequest.getEmail()).thenReturn(null);
+        when(mockRequest.getFirstname()).thenReturn(null);
+        when(mockRequest.getLastname()).thenReturn(null);
+        when(userRepositoryMock.findAll()).thenReturn(new ArrayList<>()); //there needs to be 0 users?
+        when(userRepositoryMock.findUsernameByUsername(any())).thenReturn(null); //verify there aren't any other usernames by this one
+        Users user = testService.addUser(mockRequest);
+        assertNotNull(user);
+        assertEquals(user.getUsername(), "username");
+        assertEquals(user.getPassword(), "Passw0rd$");
+        assertNull(user.getEmail());
+        assertNull(user.getFirstName());
+        assertNull(user.getLastName());
+    }
+
+    @Test
+    void test_addUser_badpassword_fail() {
+        when(mockRequest.getPassword()).thenReturn("password");
+        InvalidRequestException thrown = Assertions.assertThrows(InvalidRequestException.class, () -> {
+            testService.addUser(mockRequest);
+        });
+        verify(userRepositoryMock, times(0)).save(any());
+    }
+
+    @Test
+    void test_addUser_badPasswordNoSymbol_fail() {
+        when(mockRequest.getPassword()).thenReturn("Password1");
+        InvalidRequestException thrown = Assertions.assertThrows(InvalidRequestException.class, () -> {
+            testService.addUser(mockRequest);
+        });
+        verify(userRepositoryMock, times(0)).save(any());
     }
 
     @Test
     void isValidPassword() {
+        when(mockRequest.getPassword()).thenReturn("Passw0rd1@");
+        assertTrue(testService.isValidPassword(mockRequest.getPassword()));
     }
 
     @Test
     void isDuplicateUsername() {
+        when(userRepositoryMock.findUsernameByUsername("username")).thenReturn(null);
+        assertFalse(testService.isDuplicateUsername("username"));
     }
 
     @Test
     void isValidUsername() {
+        when(mockRequest.getUsername()).thenReturn("username");
+        assertTrue(testService.isValidUsername(mockRequest.getUsername()));
     }
 
     @Test
     void delete() {
+        testService.delete(mockUser);
+        verify(userRepositoryMock, times(1)).delete(any());
     }
 }
