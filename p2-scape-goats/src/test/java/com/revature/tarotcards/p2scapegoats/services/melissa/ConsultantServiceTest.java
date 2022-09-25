11e1569752911ceb530cf554a0866b836f05476e
Melissa <watergirl_r@yahoo.com>
2022-09-25 16:09:58 -0600

    adding more tests

---
+++ p2-scape-goats/src/test/java/com/revature/tarotcards/p2scapegoats/services/melissa/ConsultantServiceTest.java
@@ -0,0 +1,25 @@
+package com.revature.tarotcards.p2scapegoats.services.melissa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsultantServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void addConsultant() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
